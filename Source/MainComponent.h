22df11b3b754cf738f97637b6653091068efdf3c
dalbinmathew <dalbinmathew10@gmail.com>
2023-03-06 23:11:42 +0530

    added toggle buttons

--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -21,7 +21,7 @@ public:
 private:
     //==============================================================================
     // Your private member variables go here...
-
+    juce::ToggleButton tb;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
